0dc5bf63b5a564ce010136df13b8f1e5e51d1a56
Sławomir Jopek <pwnsoon@gmail.com>
2017-11-23 14:33:10 +0100

    module pattern

---
+++ module/module.js
@@ -0,0 +1,74 @@
+var moduleWithPrivate = (function(){
+    var privateVar = "privateVar";
+    var publicVar = "publicVar";
+
+    var setPrivateVar = function(value) {
+        privateVar = value;
+    };
+
+    var printPrivateVar = function() {
+        console.log(privateVar);
+    };
+
+    return {
+        set: function(value) {
+            setPrivateVar(value)
+        },
+        print: function() {
+            printPrivateVar()
+        },
+        publicVar: function() {
+            return publicVar
+        }
+    }
+})();
+
+var moduleWithPrivateV2 = (function(){
+    var object = {};
+
+    var privateVar = "privateVar";
+    var publicVar = "publicVar";
+
+    var setPrivateVar = function(value) {
+        privateVar = value;
+    };
+
+    var printPrivateVar = function() {
+        console.log(privateVar);
+    };
+
+    object.set = function(value) {
+        setPrivateVar(value)
+    };
+
+    object.print = function() {
+        printPrivateVar()
+    };
+
+    object.publicVar = function() {
+        return publicVar
+    };
+
+    return object;
+})();
+
+var moduleWithPrivateV3 = (function(){
+    var privateVar = "privateVar";
+
+    var privatePrintFunc = function() {
+        console.log(privateVar);
+    };
+
+    return {
+        printPrivateVar: function() {
+            privatePrintFunc();
+        },
+        printPublicFunc: function() {
+            printPrivateVar();  // error! scope refers to object literal not function
+        },
+        printPublicFuncV2: function() {
+            this.printPrivateVar();
+            moduleWithPrivateV3.printPrivateVar();
+        }
+    }
+})();
